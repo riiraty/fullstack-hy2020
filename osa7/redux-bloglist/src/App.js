5e25d361d3aee40d8c909d47757bc19b69ae8949
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-03-15 13:17:33 +0200

    tehtavat 7.13-7.16

--- osa7/redux-bloglist/src/App.js
+++ osa7/redux-bloglist/src/App.js
@@ -1,32 +1,75 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { notify, erase } from './reducers/notificationReducer'
-import Blog from './components/Blog'
+//import Blog from './components/Blog'
 import Notification from './components/Notification'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
 import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import userService from './services/users'
+import './App.css'
+
 import { initialize, create, like, remove } from './reducers/blogReducer'
 import { login, logout } from './reducers/userReducer'
 
-// import {
-//   BrowserRouter as Router,
-//   Switch, Route, Link
-// } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link, useParams
+} from "react-router-dom"
 
-// const Home = () => (
-//   <div> <h2>This is homepage</h2> </div>
-// )
+const User = ({ users, blogs }) => {
+  const id = useParams().id
+  const user = users.find(u => u.id === String(id))
+  console.log(blogs)
+  const usersBlogs = blogs.filter(b => b.user.id === String(id))
+  console.log(usersBlogs)
+  if(!user) {
+    return null
+  }
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h3>blogs added by user</h3>
+      <ul>
+        {usersBlogs.map(blog => 
+          <li key={blog.id}>{blog.title}</li>)}
+      </ul>
+    </div>
+  )
+}
 
-// const Bloglist = () => (
-//   <div><h2>List of blogs</h2></div>
-// )
+const Blog = ({ blogs, handleLike }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const id = useParams().id
+  const blog = blogs.find(b => b.id === String(id))
+  console.log(blog)
 
-// const Users = () => (
-//   <div> <h2>Users</h2> </div>
-// )
+  const like = () => {
+    handleLike({ blog })
+  }
+
+  if(!blog) {
+    return null
+  }
+  return (
+    <div>
+      <h2>{blog.title}</h2>
+      <p>
+        <a href={blog.url}>{blog.url}</a> <br/>
+        {blog.likes} likes <button onClick={like}>like</button> <br/>
+        added by {blog.user.name}
+      </p>
+    </div>
+  )
+}
 
 const App = () => {
   const dispatch = useDispatch()
@@ -36,6 +79,14 @@ const App = () => {
   //const [notification, setNotification] = useState(null)
   //const [user, setUser] = useState(null)
   const user = useSelector(state => state.user)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    userService
+      .getAll().then(users => {
+        setUsers( users )
+      })
+  }, [])
 
   // useEffect(() => {
   //   blogService
@@ -164,32 +215,84 @@ const App = () => {
     </Togglable>
   )
 
+  const bloglist = () => {
+    return (
+      <div id='bloglist'>
+        {blogs.sort((a, b) => {
+          return b.likes - a.likes
+        }).map(blog =>
+          <div key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+          </div>
+          //<Blog blog={blog} />
+          // <Blog
+          //   key={blog.id}
+          //   blog={blog}
+          //   user={user}
+          //   handleLike={handleLike}
+          //   handleRemove={handleRemove}
+          // />
+        )}
+      </div>
+    )
+  }
+
+  const userlist = () => {
+    return (
+      <div>
+        <h2>Users</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>blogs created</th>
+            </tr>
+            {users.map(user => 
+              <tr key={user.id}>
+                <td><Link to={`/users/${user.id}`}>{user.username}</Link></td>
+                <td>{user.blogs.length}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   const padding = { padding: 5 }
 
   return (
-    <div>
-      <h1>Bloglist</h1>
+    <Router>
+      <div>
+        <Link style={padding} to="/">blogs</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user === null ? null : <em>{user.name} logged in </em>}
+        {user === null ? loginForm() : <button onClick={handleLogout}>logout</button>}
+      </div>
+
+      <h1>Blog-app</h1>
       <Notification />
 
-      {/* <Router>
-        <div>
-          <Link ></Link>
-        </div>
 
-      </Router> */}
 
-      {user === null ?
-        loginForm() :
-        <div>
-          <p>{user.name} logged in
-            &nbsp;
-            <button onClick={handleLogout}>logout</button>
-          </p>
+      <Switch>
+        <Route path="/users/:id">
+          <User users={users} blogs={blogs}/>
+        </Route>
+        <Route path="/users">
+          {userlist()}
+        </Route>
+        <Route path="/blogs/:id">
+          <Blog blogs={blogs} handleLike={handleLike} />
+        </Route>
+        <Route path="/">
           {blogForm()}
-        </div>
-      }
+          {bloglist()}
+        </Route>
+      </Switch>
+     
 
-      <div id='bloglist'>
+      {/* <div id='bloglist'>
         {blogs.sort((a, b) => {
           return b.likes - a.likes
         }).map(blog =>
@@ -201,9 +304,9 @@ const App = () => {
             handleRemove={handleRemove}
           />
         )}
-      </div>
+      </div> */}
 
-    </div>
+    </Router>
   )
 }
 
---
+++ osa7/redux-bloglist/src/services/users.js
@@ -0,0 +1,10 @@
+//services/users.js
+import axios from 'axios'
+const baseUrl = '/api/users'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+export default { getAll }
